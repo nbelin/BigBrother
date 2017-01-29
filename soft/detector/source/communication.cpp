3c5a80e4365c3dc27bb4ea009345b861520d88c9
nbelin <nbelin@outlook.com>
2017-01-29 11:58:15 +0100

    Clean up code, indentation, old tests

--- soft/detector/source/communication.cpp
+++ soft/detector/source/communication.cpp
@@ -14,8 +14,7 @@ Communication::Communication(const int cameraId, const char * const serverIp, co
     socketAddr.sin_family = AF_INET;
     socketAddr.sin_port = htons(serverPort);
 
-    if (inet_aton(serverIp , &socketAddr.sin_addr) == 0)
-    {
+    if (inet_aton(serverIp , &socketAddr.sin_addr) == 0) {
         fprintf(stderr, "inet_aton() failed\n");
         assert(false);
     }
@@ -28,7 +27,7 @@ Communication::~Communication()
 
 void Communication::prepareMessage(const PositionMarker * pm)
 {
-    if (nbMarkers > 3){
+    if (nbMarkers > 3) {
         fprintf(stderr, "Communication::prepareMessage cannot prepare more than 4 PositionMarker\n");
     }
     posMarkers[nbMarkers++] = pm;
@@ -36,13 +35,15 @@ void Communication::prepareMessage(const PositionMarker * pm)
 
 void Communication::sendMessage()
 {
+    if (nbMarkers == 0) {
+        // don't send anything if there is no information to send!
+        return;
+    }
     int bufferLen = snprintf(buffer, BUFFLEN-1, "%d", cameraId);
-    for (int i=0; i<nbMarkers; ++i)
-    {
+    for (int i=0; i<nbMarkers; ++i) {
         int result = snprintf(buffer + bufferLen, BUFFLEN-1-bufferLen, " %d %f %f %f",
                               posMarkers[i]->pmID, (float)posMarkers[i]->x, (float)posMarkers[i]->size, posMarkers[i]->confidence);
-        if (result < 0)
-        {
+        if (result < 0) {
             fprintf(stderr, "Communication::sendMessage snprintf() failed\n");
         }
         bufferLen += result;
@@ -50,8 +51,7 @@ void Communication::sendMessage()
     buffer[BUFFLEN-1] = '\0';
 
     //send the message
-    if (sendto(socketId, buffer, strlen(buffer) , 0 , (struct sockaddr *) &socketAddr, sizeof(socketAddr))==-1)
-    {
+    if (sendto(socketId, buffer, strlen(buffer) , 0 , (struct sockaddr *) &socketAddr, sizeof(socketAddr)) == -1) {
         fprintf(stderr, "Communication::sendMessage sendTo() failed\n");
     }
 }
--- soft/detector/source/main.cpp
+++ soft/detector/source/main.cpp
@@ -14,109 +14,110 @@
 //#define VIDEO "sample/2015-06-06-172737.webm"
 //#define VIDEO "sample/2015-06-06 17.29.40.mov"
 //#define VIDEO "sample/2015-06-06-174454.webm"
-#define VIDEO "../../sample/test.mp4"
+//#define VIDEO "../../sample/test.mp4"
+#define VIDEO 0
 
 int main(int argc, char* argv[]) {
-  std::cout << "GO" << std::endl;
-  cv::VideoCapture cap(VIDEO); // open the default camera
-  if(!cap.isOpened())  // check if we succeeded
-    return -1;
-  
-  cv::Mat frame;
-  cv::Mat hsv;
-  cap >> frame;
-  
-  Image3D image(frame.cols, frame.rows, NULL);
-
-  Rectangle dummyRect;
-  //Marker marker1(image, false, yellowRect, redRect, dummyRect);
-  Marker marker1(image, true, darkBlueRect, magentaRect, dummyRect);
-  PositionMarker pm1(0);
-  Marker marker2(image, false, redRect, yellowRect, dummyRect);
-  PositionMarker pm2(1);
-  Marker marker3(image, true, greenRect, blueRect, dummyRect);
-  PositionMarker pm3(2);
-  Marker marker4(image, true, blueRect, greenRect, dummyRect);
-  PositionMarker pm4(3);
-
-  Communication comm(CAMERA_ID, SERVER_IP, SERVER_PORT);
-
-  std::cout << "start loop" << std::endl;
-  int count = 0;
-  while(1) {
-    cap >> frame; // get a new frame from camera
-    cv::cvtColor(frame, hsv, CV_BGR2Luv);
-    image.setData(hsv.data);
-    image.id = count;
-
-    if (count % 20 == 0)
-        std::cout << "NEXT POS " << count << std::endl;
-    bool result;
-
-    result = marker1.getNextPos(image, pm1);
-    if (!result) {
-        std::cout << "POS " << count << std::endl;
-        std::cout << "NOP (1)" << std::endl;
+    std::cout << "GO" << std::endl;
+    cv::VideoCapture cap(VIDEO); // open the default camera
+    if(!cap.isOpened()) {  // check if we succeeded
+        std::cout << "Failed to open video" << std::endl;
+        return -1;
     }
 
-//    result = marker2.getNextPos(image, pm2);
-//    if (!result) {
-//        std::cout << "POS " << count << std::endl;
-//        std::cout << "NOP (2)" << std::endl;
-//    }
+    cv::Mat frame;
+    cv::Mat hsv;
+    cap >> frame;
 
-//    result = marker3.getNextPos(image, pm3);
-//    if (!result) {
-//        std::cout << "POS " << count << std::endl;
-//        std::cout << "NOP (3)" << std::endl;
-//    }
+    // This first (dummy) image is used to initialize buffers in Classes
+    Image3D image(frame.cols, frame.rows, NULL);
 
-//    result = marker4.getNextPos(image, pm4);
-//    if (!result) {
-//        std::cout << "POS " << count << std::endl;
-//        std::cout << "NOP (4)" << std::endl;
-//    }
+    Rectangle dummyRect;
+    //Marker marker1(image, false, yellowRect, redRect, dummyRect);
+    Marker marker1(image, true, darkBlueRect, magentaRect, dummyRect);
+    PositionMarker pm1(0);
+#if 0
+    Marker marker2(image, false, redRect, yellowRect, dummyRect);
+    PositionMarker pm2(1);
+    Marker marker3(image, true, greenRect, blueRect, dummyRect);
+    PositionMarker pm3(2);
+    Marker marker4(image, true, blueRect, greenRect, dummyRect);
+    PositionMarker pm4(3);
+#endif
+    Communication comm(CAMERA_ID, SERVER_IP, SERVER_PORT);
 
-     Image &mask = marker1.masks[0];
-     for (unsigned int im=0; im<mask.height; ++im) {
-       for (unsigned int jm=0; jm<mask.width; ++jm) {
-        if (mask.getValue(im, jm) == 1)
-          cv::line(frame, cv::Point(jm, im), cv::Point(jm, im), cv::Scalar(255, 0, 0));
-       }
-     }
+    std::cout << "start loop" << std::endl;
+    int count = 0;
+    while(1) {
+        cap >> frame; // get a new frame from camera
+        cv::cvtColor(frame, hsv, CV_BGR2Luv);
+        image.setData(hsv.data);
+        image.id = count;
 
-//    Image &mask1 = marker3.masks[0];
-//    for (unsigned int im=0; im<mask1.height; ++im) {
-//      for (unsigned int jm=0; jm<mask1.width; ++jm) {
-//        if (mask1.getValue(im, jm) == 1)
-//          cv::line(frame, cv::Point(jm, im), cv::Point(jm, im), cv::Scalar(255, 0, 255));
-//      }
-//    }
+        if (count % 20 == 0)
+            std::cout << "NEXT POS " << count << std::endl;
+        bool result;
 
-    cv::rectangle(frame, cv::Point(pm1.x - pm1.size/2, pm1.minI), cv::Point(pm1.x + pm1.size/2, pm1.maxI), cv::Scalar(0, 0, 0));
-    cv::rectangle(frame, cv::Point(pm1.x - pm1.size/2 + 1, pm1.minI + 1), cv::Point(pm1.x + pm1.size/2 - 1, pm1.maxI - 1), cv::Scalar(0, 0, 0));
-    cv::rectangle(frame, cv::Point(pm2.x - pm2.size/2, pm2.minI), cv::Point(pm2.x + pm2.size/2, pm2.maxI), cv::Scalar(0, 0, 0));
-    cv::rectangle(frame, cv::Point(pm2.x - pm2.size/2 + 1, pm2.minI + 1), cv::Point(pm2.x + pm2.size/2 - 1, pm2.maxI - 1), cv::Scalar(0, 0, 0));
-    cv::rectangle(frame, cv::Point(pm3.x - pm3.size/2, pm3.minI), cv::Point(pm3.x + pm3.size/2, pm3.maxI), cv::Scalar(0, 0, 255));
-    cv::rectangle(frame, cv::Point(pm3.x - pm3.size/2 + 1, pm3.minI + 1), cv::Point(pm3.x + pm3.size/2 - 1, pm3.maxI - 1), cv::Scalar(0, 0, 0));
-    cv::rectangle(frame, cv::Point(pm4.x - pm4.size/2, pm4.minI), cv::Point(pm4.x + pm4.size/2, pm4.maxI), cv::Scalar(0, 0, 255));
-    cv::rectangle(frame, cv::Point(pm4.x - pm4.size/2 + 1, pm4.minI + 1), cv::Point(pm4.x + pm4.size/2 - 1, pm4.maxI - 1), cv::Scalar(0, 0, 0));
-//    cv::line(frame, cv::Point(pm1.x - pm1.size/2, 183), cv::Point(pm1.x + pm1.size/2, 211), cv::Scalar(0, 0, 255));
-//    cv::line(frame, cv::Point(565, 220), cv::Point(575, 230), cv::Scalar(0, 0, 255));
-    cv::imshow("img", frame);
-    cv::waitKey(100);
-    if (count > 472 && count < 475)
-       cv::waitKey(1000);
-    // if (count > 2250)
-    //   cv::waitKey(2);
-    
-    count++;
+        result = marker1.getNextPos(image, pm1);
+        if (result) {
+            comm.prepareMessage(&pm1);
+        } else {
+            std::cout << "POS " << count << std::endl;
+            std::cout << "NOP (1)" << std::endl;
+        }
 
-    comm.prepareMessage(&pm1);
-    comm.prepareMessage(&pm2);
-    comm.prepareMessage(&pm3);
-    comm.prepareMessage(&pm4);
-    comm.sendMessage();
-    comm.resetMessage();
-  }
+#if 0 // simple conf: 1 marker to detect
+        result = marker2.getNextPos(image, pm2);
+        if (result) {
+            comm.prepareMessage(&pm2);
+        } else {
+            std::cout << "POS " << count << std::endl;
+            std::cout << "NOP (2)" << std::endl;
+        }
+
+        result = marker3.getNextPos(image, pm3);
+        if (result) {
+            comm.prepareMessage(&pm3);
+        } else {
+            std::cout << "POS " << count << std::endl;
+            std::cout << "NOP (3)" << std::endl;
+        }
+
+        result = marker4.getNextPos(image, pm4);
+        if (result) {
+            comm.prepareMessage(&pm4);
+        } else {
+            std::cout << "POS " << count << std::endl;
+            std::cout << "NOP (4)" << std::endl;
+        }
+#endif
+
+        // debug detected pixels for given marker/color
+        Image &mask = marker1.masks[0];
+        for (unsigned int im=0; im<mask.height; ++im) {
+            for (unsigned int jm=0; jm<mask.width; ++jm) {
+                if (mask.getValue(im, jm) == 1) {
+                    cv::line(frame, cv::Point(jm, im), cv::Point(jm, im), cv::Scalar(255, 0, 0));
+                }
+            }
+        }
+
+        // show detected markers
+        cv::rectangle(frame, cv::Point(pm1.x - pm1.size/2, pm1.minI), cv::Point(pm1.x + pm1.size/2, pm1.maxI), cv::Scalar(0, 0, 0));
+        cv::rectangle(frame, cv::Point(pm1.x - pm1.size/2 + 1, pm1.minI + 1), cv::Point(pm1.x + pm1.size/2 - 1, pm1.maxI - 1), cv::Scalar(0, 0, 0));
+        cv::rectangle(frame, cv::Point(pm2.x - pm2.size/2, pm2.minI), cv::Point(pm2.x + pm2.size/2, pm2.maxI), cv::Scalar(0, 0, 0));
+        cv::rectangle(frame, cv::Point(pm2.x - pm2.size/2 + 1, pm2.minI + 1), cv::Point(pm2.x + pm2.size/2 - 1, pm2.maxI - 1), cv::Scalar(0, 0, 0));
+        cv::rectangle(frame, cv::Point(pm3.x - pm3.size/2, pm3.minI), cv::Point(pm3.x + pm3.size/2, pm3.maxI), cv::Scalar(0, 0, 255));
+        cv::rectangle(frame, cv::Point(pm3.x - pm3.size/2 + 1, pm3.minI + 1), cv::Point(pm3.x + pm3.size/2 - 1, pm3.maxI - 1), cv::Scalar(0, 0, 0));
+        cv::rectangle(frame, cv::Point(pm4.x - pm4.size/2, pm4.minI), cv::Point(pm4.x + pm4.size/2, pm4.maxI), cv::Scalar(0, 0, 255));
+        cv::rectangle(frame, cv::Point(pm4.x - pm4.size/2 + 1, pm4.minI + 1), cv::Point(pm4.x + pm4.size/2 - 1, pm4.maxI - 1), cv::Scalar(0, 0, 0));
+
+        cv::imshow("img", frame);
+        cv::waitKey(100);
+
+        count++;
+
+        comm.sendMessage();
+        comm.resetMessage();
+    }
 }
