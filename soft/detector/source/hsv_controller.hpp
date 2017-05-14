ba81579109e338e434e4319b580c57a62f49a8bf
nbelin <nbelin@outlook.com>
2017-05-14 17:42:59 +0200

    Naive parallel HSV color conversion. 4 threads are created each time.. but that's fine for now

--- soft/detector/source/hsv_controller.hpp
+++ soft/detector/source/hsv_controller.hpp
@@ -1,24 +1,50 @@
 #ifndef HSV_CONTROLLER_HPP
 #define HSV_CONTROLLER_HPP
 
+#include <vector>
+#include <thread>
 #include "data.hpp"
 
 class HSVController {
 private:
     Data& data;
+    std::vector<std::thread> threads;
+    std::vector<cv::Mat> src_mats;
+    std::vector<cv::Mat> dst_mats;
 public:
     HSVController(Data& data)
         : data(data) {
+        threads.resize(4);
+        src_mats.resize(4);
+        dst_mats.resize(4);
     }
 
     void update(void) {
         if ((data.color_choice & Color::HSV) == 0) {
             return;
         }
-        cv::cvtColor(data.frame, data.hsv, CV_BGR2HSV);
-        assert(data.hsv.data != nullptr);
+        //cv::cvtColor(data.frame, data.hsv, CV_BGR2HSV);
+        data.hsv = data.frame.clone();
+        for (size_t i=0; i<threads.size(); ++i) {
+            unsigned int height = data.frame.rows / 4;
+            unsigned int row = height * i;
+            cv::Rect rect = cv::Rect(0, row, data.frame.cols, height);
+            src_mats[i] = data.frame(rect);
+            dst_mats[i] = data.hsv(rect);
+            threads[i] = std::thread(jobCvtColor, &src_mats[i], &dst_mats[i]);
+        }
+
+        for (size_t i=0; i<threads.size(); ++i) {
+            threads[i].join();
+        }
+
         data.image.setData(data.hsv.data);
     }
+
+    static void jobCvtColor(const cv::Mat * src, cv::Mat * dst) {
+        cv::cvtColor(*src, *dst, CV_BGR2HSV);
+    }
+
 };
 
 #endif //HSV_CONTROLLER_HPP
