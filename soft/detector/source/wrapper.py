094033d8e2da258d3a7e8b4f6ee8e1026ca3996f
nbelin <nbelin@outlook.com>
2017-05-14 13:56:26 +0200

    The detector code has now its own python wrapper. Listens to the triangulator (multicast) which tells when the game starts. Prepare a delayed shutdown (110 secs), and run in loop the detector C++ main code with the proper arguments (list of markers + output avi video)

---
+++ soft/detector/source/wrapper.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import socket
+import sys
+import time
+import subprocess
+import os
+import struct
+
+####################
+####### main #######
+####################
+
+# global var config
+config_file_name = "../config/config_wrapper.txt"
+UDP_PORT = 0
+UDP_ADDR = ""
+
+# parse configuration
+try:
+	config_file = open(config_file_name)
+	for line in config_file:
+		if line.startswith("#") or line == "\n":
+			continue
+		line = line.rstrip('\n')
+		token, value = line.split("=")
+		if token == "UDP_PORT":
+			UDP_PORT = int(value)
+		elif token == "UDP_ADDR":
+			UDP_ADDR = value
+		else:
+			raise SyntaxError("Unrecognized token:" + token)
+	if UDP_PORT == 0:
+		raise SyntaxError("UDP_PORT has not been defined")
+except Exception as e:
+	print repr(e)
+	sys.exit("Error reading config file " + config_file_name)
+config_file.close()
+
+
+# prepare socket
+sock_triang = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP MULTICAST
+sock_triang.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_triang.bind((UDP_ADDR, UDP_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(UDP_ADDR), socket.INADDR_ANY)
+sock_triang.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+sock_triang.settimeout(60)
+
+nb_retry = 0
+arg_marker = ""
+
+# wait for game to start
+print ""
+print "Listenning port " + str(UDP_PORT) + "..."
+while True:
+	try:
+		print "nb_retry: ", nb_retry
+		data = sock_triang.recv(1024)
+		print "received: ", data
+		if data.startswith("GO "):
+			arg_marker = data.split(" ")[1]
+			break
+		print "wrong message!"
+	except Exception as e:
+		print repr(e)
+		pass
+	nb_retry += 1
+
+# enable raspicam, let the code continue even if the command fails
+try:
+	print "run: sudo modprobe bcm2835-v4l2"
+	subprocess.call(["sudo", "modprobe bcm2835-v4l2"])
+except Exception as e:
+	print repr(e)
+# delay a shutdown, let the code continue even if the command fails
+try:
+	print "run: sleep 110 && sudo shutdown -h now &"
+	subprocess.call("sleep 110 && sudo shutdown -h now &")
+except Exception as e:
+	print repr(e)
+
+# prepare directories
+output_dir_template = "../results/"
+try:
+	print "create ", output_dir_template
+	os.mkdir(output_dir_template)
+except Exception as e:
+	print repr(e)
+
+# try in loop to create a new dir for this game
+while True:
+	output_dir = output_dir_template + "game_" + str(time.time()) + "/"
+	try:
+		print "create ", output_dir
+		os.mkdir(output_dir)
+	except Exception as e:
+		print repr(e)
+		pass
+	break
+
+# run main process
+command_line_template = "../build/detector -m=" + arg_marker
+print "cmd_line_template: ", command_line_template
+for i in range(20):
+	arg_output = output_dir + "video" + str(i) + ".avi"
+	command_line = command_line_template + " -o=" + arg_output
+	print "run: ", command_line
+	ret = subprocess.call(command_line, shell=True)
+	print "returned: ", ret
+	if ret == 0:
+		break
+
+print "end"
